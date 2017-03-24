409b83f1a740a5f42d486569b6540c31fe24c577
Ben Hsieh <t_hsieb@SFOC02QVAV0G8WP.local>
2017-03-24 00:20:39 -0700

    add github issues service

---
+++ src/app/services/github/issue-data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+@Injectable()
+export class IssueDataService {
+
+  constructor(
+      private http: Http
+  ) { }
+
+  getIssues(): any {
+    this.http.get('https://api.github.com/repos/angular/angular/issues')
+  }
+
+
+
+}
